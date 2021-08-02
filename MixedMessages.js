22bfbd58514b659170f203b7b9398a90a428f9f0
Jack Willis <jackwillis322@gmail.com>
2021-08-02 12:22:57 +0100

    Completed the Mixed Messages Program

--- MixedMessages.js
+++ MixedMessages.js
@@ -4,7 +4,8 @@
 const animals = ["Squirrel","Dog","Chimpanzee","Ox","Lion","Panda","Walrus","Otter","Mouse","Kangaroo","Goat","Horse","Monkey","Cow","Koala","Mole","Elephant","Leopard","Hippopotamus","Giraffe","Fox","Coyote","Hedgehog","Sheep","Deer"]
 const geoPrefix = ["British","Ugandan","Equitorian","Sub-Saharan","Austrailian","North American","German","French","Portugese","Scandinavian","Chinese","Fresh Water","Himalayan","Andoran","Brazilian","Artic","Guinean","Coastal"]
 const genPrefix = ["Great","Horned","Elder","Spotted","Aged","Thrasing","Mute","Spine Ridged","Burrowing","Domesticated","Household","Hunting"]
-const quality = ["Length","Height","Weight"]
+const quality = ["height","weight","lifespan"]
+const unit =["meters","kilograms","years"]
 
 const generateAnimal = () => {
     const generic = genPrefix[Math.floor(Math.random()*genPrefix.length)]
@@ -12,6 +13,25 @@ const generateAnimal = () => {
     const animal = animals[Math.floor(Math.random()*animals.length)]
     return `${geographic} ${generic} ${animal}`
 }
+/*
 for (i=0;i<10;i++) {
     console.log(generateAnimal())
 }
+*/
+const generateHeight = () => {
+    return (1.50 + Math.floor(Math.random()*250)/100)
+}
+const generateWeight = () => {
+    return (80 + Math.floor(Math.random()*200))
+}
+const generateLifespan = () => {
+    return 20 + Math.floor(Math.random()*100)
+}
+const funcArray = [generateHeight,generateWeight,generateLifespan]
+
+const generatePhrase = () => {
+    measurementChoice = Math.floor(Math.random()*3)
+    console.log(`The ${generateAnimal()} has a ${quality[measurementChoice]} that can reach a whopping ${funcArray[measurementChoice]()} ${unit[measurementChoice]}.`)
+}
+
+generatePhrase()
